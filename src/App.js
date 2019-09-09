af332d56dd591101db2fd0dcf520960d757bfbf0
Kelly Moreira <moreira.kd@outlook.com>
2019-09-09 15:28:08 -0400

    step 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import ProductContext from './contexts/ProductContext.js';
 
 // Components
 import Navigation from './components/Navigation';
@@ -8,19 +9,26 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
+	// [products] will go to ProductValue
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
+
+	// [addItem] will go to ProductContext
 	const addItem = item => {
 		setCart([...cart, item]);
 	};
 
 	return (
+
+		<ProductContext.Provider value= {{products, addItem}}>
 		<div className="App">
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route
+
+			{/* Before */}
+			{/* <Route
 				exact
 				path="/"
 				render={() => (
@@ -29,13 +37,21 @@ function App() {
 						addItem={addItem}
 					/>
 				)}
-			/>
+			/> */}
+
+			{/* After */}
+<Route exact path="/" component={Products} />
+
+{/* The after route look cleaner bc we no longer need to pass down the products and additem props.
+We no longer have to do this bc the products and additems props are passed in as values inside 
+ProductContext.Provider on line 24. */}
 
 			<Route
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
 		</div>
+		</ProductContext.Provider>
 	);
 }
 
